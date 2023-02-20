53f7455ae0eec5a50e5cbaaac4cfaa7a3598fbf2
j1642 <60148902+j1642@users.noreply.github.com>
2023-02-20 13:13:52 -0500

    MAINT: Refactor messages to user

--- treasury_yield_curve.py
+++ treasury_yield_curve.py
@@ -17,8 +17,8 @@ def prompt_date():
     stock exchanges are open), not during a weekend or holiday.
     """
     try:
-        print('For which trading day do you want to view the yield curve?',
-              'Response format must be YYYY-MM-DD.')
+        print('For which trading day do you want to view the yield curve?'
+              ' (YYYY-MM-DD)')
         date = input('>>> ')
         assert len(date) == 10 and date[:4].isdigit() and date[4] == '-' \
             and date[5:7].isdigit() and date[7] == '-' and date[8:10].isdigit()
@@ -41,7 +41,6 @@ def parse_xml_for_rates():
     url.append(year)
     url.append(month)
     url = ''.join(url)
-    print('Connecting to treasury.gov...')
 
     content_start_index = 0
     # Treasury changed XML structure on Oct. 19, 2022.
@@ -50,12 +49,13 @@ def parse_xml_for_rates():
     if IS_OLD_XML_FORMAT:
         content_start_index = 1
 
+    print('Connecting to treasury.gov...')
     with urllib.request.urlopen(url) as page:
         print('Reading XML...')
         tree = ET.parse(page)
     root = tree.getroot()
 
-    # The XML tages in the tree variable are all prepended by a URL.
+    # The XML tags in the tree variable are all prepended by a URL.
     for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
         for content in entry.find('{http://www.w3.org/2005/Atom}content'):
             if content[content_start_index].text[:10] == date:
@@ -70,7 +70,13 @@ def parse_xml_for_rates():
 
 def plot_rates(rates_and_date: tuple):
     """Generate a plot of the selected day's interest rates."""
-    rates, date = rates_and_date
+    try:
+        rates, date = rates_and_date
+    except TypeError:
+        print('Are you sure that was a trading day? If so, this tool is '
+            'not configured to read Treasury data from before 2019, '
+            'roughly.')
+        return
     year, month, day = date.split('-')
     x_values = (1 / 12, 1 / 6, 1 / 4, 1 / 3, 1 / 2, 1, 2, 3, 5, 7, 10, 20, 30)
     # Treasury added a new data point on Oct. 19, 2022, the 4 month bill.
@@ -86,8 +92,9 @@ def plot_rates(rates_and_date: tuple):
         plt.plot(x_values, rates, marker='o')
         plt.show()
     except (IndexError, ValueError):
-        print(f'\nNo rates found for {date}. Are you sure it was a'
-               'trading day?')
+        print('This tool is not configured to read Treasury data from '
+            'before 2019, roughly.')
+        return
 
 
 plot_rates(parse_xml_for_rates())
