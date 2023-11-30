73b9ceebe6a4890a1a0534d1ee4f8ec94a55d330
j1642 <60148902+j1642@users.noreply.github.com>
2023-11-30 11:21:20 -0500

    ENH: Simple parsing works for nested JSON objects

--- json_parser.py
+++ json_parser.py
@@ -49,44 +49,79 @@ def parse_array(i, tokens):
         if tokens[i] == "]":
             i += 1
             return i, values
+        elif tokens[i] == "{":
+            i += 1
+            i, obj = parse_obj(i, tokens)
+            values.append(obj)
         elif tokens[i].isalnum():
             values.append(tokens[i])
             i += 1
+            if tokens[i] != "," and tokens[i] != "]":
+                print("next tok=", tokens[i], i)
+                print("next next tok=", tokens[i+1], i+1)
+                raise ValueError(
+                        "parse_array() did not find delimiter or end of array"
+                )
         elif tokens[i] == "[":
             i += 1
-            values.append(parse_array(i, tokens))
-        elif tokens[i] == "{":
-            i += 1
-            values.append(parse_obj(i, tokens))
+            i, arr = parse_array(i, tokens)
+            values.append(arr)
         else:
             i += 1
     if i >= len(tokens):
         raise ValueError("parse_array() did not find end of array")
 
 def parse_obj(i, tokens):
+    print("tokens len and id:", len(tokens), id(tokens))
     obj = {}
-    i = 0
+    key = ""
     while i < len(tokens):
+        token = tokens[i]
         if token == "{":
-            x = parse_obj()
+            i, child_obj = parse_obj(i + 1, tokens)
+            if key == "":
+                print("i where key is empty=", i, child_obj)
+            assert key != "" or i >= len(tokens)
+            if i >= len(tokens):
+                return i, child_obj
+            obj[key] = child_obj
+            key = ""
         elif token == "}":
-            return obj
+            return i, obj
         elif token == "[":
-            x = parse_array(i, tokens)
-        #elif token == "]":
-            # Should only be found in build_array()
-            #pass #assert False
+            i, arr = parse_array(i + 1, tokens)
+            assert key != ""
+            obj[key] = arr
+            key = ""
         elif token == ":":
             # expect value or array
-            #val = next()
-            #obj[key] = val
-            pass
+            assert key != ""
         elif token == ",":
-            pass
+            # parse_array() should find all array delimiters
+            key = ""
         elif token in ("null", "true", "false"):
-            pass
+            obj[key] = token
+            key = ""
+        else:
+            if key == "":
+                key = token
+            else:
+                if token.isdigit():
+                    obj[key] = int(token)
+                else:
+                    obj[key] = token
+                key = ""
         i += 1
+    return i, obj
 
 if __name__ == "__main__":
     json = """{"status":"SpaceTraders is currently online and available to play","version":"v2.1.2","resetDate":"2023-11-18","description":"SpaceTraders is a headless API and fleet-management game where players can work together or against each other to trade, explore, expand, and conquer in a dynamic and growing universe. Build your own UI, write automated scripts, or just play the game from the comfort of your terminal. The game is currently in alpha and is under active development.","stats":{"agents":981,"ships":2593,"systems":8498,"waypoints":171410},"leaderboards":{"mostCredits":[{"agentSymbol":"WHYANDO","credits":49033063},{"agentSymbol":"SG-1-DEVX","credits":23554646},{"agentSymbol":"WHYAIR","credits":23447152},{"agentSymbol":"EMBERCOM","credits":19870328},{"agentSymbol":"SAFPLUSPLUS","credits":16114234},{"agentSymbol":"SIKAYN","credits":14400580},{"agentSymbol":"SG-1-DEVX2","credits":11074824},{"agentSymbol":"CTRI-U-","credits":10381460},{"agentSymbol":"ESEIDEL","credits":4143143},{"agentSymbol":"RDTST1","credits":3011010},{"agentSymbol":"RDTST3","credits":3000019},{"agentSymbol":"RUTHLESSDUCK","credits":2986999},{"agentSymbol":"PHANTASM","credits":2838114},{"agentSymbol":"BLACKRAT","credits":1904066},{"agentSymbol":"CTRI-V-","credits":890783}],"mostSubmittedCharts":[]},"serverResets":{"next":"2023-12-02T16:00:00.000Z","frequency":"fortnightly"},"announcements":[{"title":"Server Resets","body":"We will be doing complete server resets frequently during the alpha to deploy fixes, add new features, and balance the game. Resets will typically be conducted on Saturday mornings. Previous access tokens will no longer be valid after the reset and you will need to re-register your agent. Take this as an opportunity to try and make it to the top of the leaderboards!"},{"title":"Support Us","body":"Supporters of SpaceTraders can reserve their agent call sign between resets. Consider donating to support our development: https://donate.stripe.com/28o29m5vxcri6OccMM"},{"title":"Discord","body":"Our Discord community is very active and helpful. Share what you're working on, ask questions, and get help from other players and the developers: https://discord.com/invite/jh6zurdWk5"}],"links":[{"name":"Website","url":"https://spacetraders.io/"},{"name":"Documentation","url":"https://docs.spacetraders.io/"},{"name":"Playground","url":"https://docs.spacetraders.io/playground"},{"name":"API Reference","url":"https://spacetraders.stoplight.io/docs/spacetraders/"},{"name":"OpenAPI Spec - Bundled","url":"https://stoplight.io/api/v1/projects/spacetraders/spacetraders/nodes/reference/SpaceTraders.json?fromExportButton=true&snapshotType=http_service&deref=optimizedBundle"},{"name":"OpenAPI Spec - Source","url":"https://github.com/SpaceTradersAPI/api-docs/blob/main/reference/SpaceTraders.json"},{"name":"Discord","url":"https://discord.com/invite/jh6zurdWk5"},{"name":"Support Us","url":"https://donate.stripe.com/28o29m5vxcri6OccMM"},{"name":"Report Issues","url":"https://github.com/SpaceTradersAPI/api-docs/issues"},{"name":"Wiki","url":"https://github.com/SpaceTradersAPI/api-docs/wiki"},{"name":"Account Portal (Coming Soon)","url":"https://my.spacetraders.io/"},{"name":"Twitter","url":"https://twitter.com/SpaceTradersAPI"}]}"""
+
     tokens = lex(json)
+    i = 0
+    for tok in tokens[:200]:
+        print(i, tok)
+        i += 1
+    print("len(tokens) =", len(tokens))
+    obj = parse_obj(0, tokens)
+    print(obj)
--- test/test_json_parser.py
+++ test/test_json_parser.py
@@ -12,3 +12,21 @@ def test_lex_array():
     tokens = json_parser.lex(json)
     assert tokens == ["{", "list", ":",
         "[", "true", ",", "false", ",", "null", ",", "21", "]", "}"] 
+
+def test_parsing():
+    json = """{"status":"SpaceTraders","version":"v2.1.2","resetDate":
+"2023-11-18","description":"SpaceTraders","stats":{"agents":1000,"ships":2637,
+"systems":8498,"waypoints":171410}"""
+    tokens = json_parser.lex(json)
+    _, obj = json_parser.parse_obj(0, tokens)
+    assert obj == {"status": "SpaceTraders",
+                   "version": "v2.1.2",
+                   "resetDate": "2023-11-18",
+                   "description": "SpaceTraders",
+                   "stats": {
+                       "agents": 1000,
+                       "ships": 2637,
+                       "systems": 8498,
+                       "waypoints": 171410
+                   }
+   }
