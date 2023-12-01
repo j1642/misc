6d2252725bda6daad76b516ae9a4bcc9a1bf9725
j1642 <60148902+j1642@users.noreply.github.com>
2023-11-30 21:06:16 -0500

    MNT: Change parser to accept JSON file as a command line argument, remove token alteration from parse_obj()

--- json_parser.py
+++ json_parser.py
@@ -1,4 +1,5 @@
 """Convert a JSON string to a python object"""
+import sys
 
 def next(i, json):
     # Get next token
@@ -109,17 +110,16 @@ def parse_obj(tokens, i=1):
             if key == "":
                 key = token
             else:
-                if token.isdigit():
-                    obj[key] = int(token)
-                else:
-                    obj[key] = token
+                obj[key] = token
                 key = ""
         i += 1
     return i, obj
 
 if __name__ == "__main__":
-    json = """{"status":"SpaceTraders","version":"v2.1.2","resetDate":"2023-11-18","description":"SpaceTraders","stats":{"agents":981,"ships":2593,"systems":8498,"waypoints":171410},"leaderboards":{"mostCredits":[{"agentSymbol":"WHYANDO","credits":49033063},{"agentSymbol":"SG-1-DEVX","credits":23554646},{"agentSymbol":"WHYAIR","credits":23447152},{"agentSymbol":"EMBERCOM","credits":19870328},{"agentSymbol":"SAFPLUSPLUS","credits":16114234},{"agentSymbol":"SIKAYN","credits":14400580},{"agentSymbol":"SG-1-DEVX2","credits":11074824},{"agentSymbol":"CTRI-U-","credits":10381460},{"agentSymbol":"ESEIDEL","credits":4143143},{"agentSymbol":"RDTST1","credits":3011010},{"agentSymbol":"RDTST3","credits":3000019},{"agentSymbol":"RUTHLESSDUCK","credits":2986999},{"agentSymbol":"PHANTASM","credits":2838114},{"agentSymbol":"BLACKRAT","credits":1904066},{"agentSymbol":"CTRI-V-","credits":890783}],"mostSubmittedCharts":[]},"serverResets":{"next":"2023-12-02T16:00:00.000Z","frequency":"fortnightly"},"announcements":[{"title":"Server Resets","body":"We"},{"title":"Support Us","body":"Supporters"},{"title":"Discord","body":"Our"}],"links":[{"name":"Website","url":"https://spacetraders.io/"},{"name":"Documentation","url":"https://docs.spacetraders.io/"},{"name":"Playground","url":"https://docs.spacetraders.io/playground"},{"name":"API Reference","url":"https"},{"name":"OpenAPI Spec - Bundled","url":"https://s"},{"name":"OpenAPI Spec - Source","url":"https://github.com/SpaceTradersAPI/api-docs/blob/main/reference/SpaceTraders.json"},{"name":"Discord","url":"https://discord.com/invite/jh6zurdWk5"},{"name":"Support Us","url":"https://donate.stripe.com/28o29m5vxcri6OccMM"},{"name":"Report Issues","url":"https://github.com/SpaceTradersAPI/api-docs/issues"},{"name":"Wiki","url":"https://github.com/SpaceTradersAPI/api-docs/wiki"},{"name":"Account Portal (Coming Soon)","url":"https://my.spacetraders.io/"},{"name":"Twitter","url":"https://twitter.com/SpaceTradersAPI"}]}"""
-
+    with open(sys.argv[1]) as f:
+        json = f.read()
+    # multiple files end in Unicode 10 control code
+    json = json[:-1]
     tokens = lex(json)
     obj = parse_obj(tokens)
     print(obj[1])
