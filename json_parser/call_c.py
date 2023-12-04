296c5a72ea72263cab02b4684738bc72d6e48626
j1642 <60148902+j1642@users.noreply.github.com>
2023-12-03 22:33:27 -0500

    Rewrite lexing function for JSON numbers in C

--- json_parser/call_c.py
+++ json_parser/call_c.py
@@ -1,4 +1,7 @@
 import ctypes
 
 parse_funcs = ctypes.CDLL('./parse_funcs.so')
-print(parse_funcs.parse_num(212, "Σ json text".encode('utf-8')))
+s = "Σ json -1.234e-5x"
+actual = parse_funcs.parse_num(7, s.encode('utf-8'))
+print("actual:", actual, f"({s[actual]})")
+print("expected:", len(s) - 1)
--- json_parser/parse_funcs.c
+++ json_parser/parse_funcs.c
@@ -1,48 +1,49 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-//#include <stdio.h>
 
-void parse_num(int i, char *json) {
-    char len = 14;
-    printf("i=%d, json=%s\n", i, json);
-    printf("sizeof(json)=%lu\n", sizeof(json));
-    fprintf(stderr, "%s", "error: error text\n"); // does not exit by itself
-    printf("json bytes = ");
-    for (int i = 0; i < len; i++) {
-        printf("%d ", json[i]);
+int parse_num(int i, char *json) {
+    int len = strlen(json);
+    //printf("i=%d, json=%s\n", i, json); // prints sigma correctly
+    wchar_t wc_json[len * 2];
+    // Count characters in a string, accounting for multi-bytes
+    size_t wc_len = mbstowcs(wc_json, json, len * 2);
+
+    if (wc_json[i] == '-') {
+        assert('0' <= wc_json[i + 1] && wc_json[i + 1] <= '9');
+        i++;
+    } else {
+        assert('0' <= wc_json[i] && wc_json[i] <= '9');
     }
-    printf("\n");
-    //char s[3];
-    for (int i = 0; i < len; i++) {
-//snprintf(s, 3, "%d", json[i]);
-        //printf("%s ", s);
-        //printf("%d ", json[i] + '0');
-        //printf("%d ", (char)json[i]);
-        printf("%c ", (char)json[i]);
+    if (wc_json[i] == '0' && wc_json[i + 1] != '.') {
+        fprintf(stderr, "%s", "error: JSON numbers cannot have leading zeroes\n");
+        exit(1);
     }
-    printf("\n");
+    while ('0' <= wc_json[i] && wc_json[i] <= '9') {
+        i++;
+    }
+    if (wc_json[i] == '.') {
+        if (wc_json[i + 1] < '0' || '9' < wc_json[i + 1]) {
+            fprintf(stderr, "%s", "error: decimal point not followed by digits\n");
+            exit(1);
+        }
+        i++;
+        while ('0' <= wc_json[i] && wc_json[i] <= '9') {
+            i++;
+        }
+        if (wc_json[i] == 'e' || wc_json[i] == 'E') {
+            //printf("entered exponent if\n");
+            i++;
+            if (wc_json[i] == '+' || wc_json[i] == '-') {
+                i++;
+                while ('0' <= wc_json[i] && wc_json[i] <= '9') {
+                    i++;
+                }
+            }
+        }
+    }
+    if ((unsigned long)i > wc_len) {
+        fprintf(stderr, "%s", "error: escaped end of JSON string\n");
+        exit(1);
+    }
+    return i;
 }
-
-//        elif json[i].isdigit() or (json[i] == "-" and json[i + 1].isdigit()):
-//            if json[i] == "-":
-//                i += 1
-//            # Allow floats like 0.1
-//            if json[i] == "0" and json[i + 1] != ".":
-//                raise ValueError("JSON numbers cannot have leading zeroes")
-//            while json[i].isdigit():
-//                i += 1
-//            # Check for floats
-//            if json[i] == ".":
-//                if not json[i + 1].isdigit():
-//                    raise ValueError(f"decimal point not followed by digits: idx={i}")
-//                i += 1
-//                while json[i].isdigit():
-//                    i += 1
-//                # Check for scientific notation
-//                if json[i] == "e" or json[i] == "E":
-//                    i += 1
-//                    if json[i] == "+" or json[i] == "-":
-//                        i += 1
-//                    while json[i].isdigit():
-//                        i += 1
-//            tokens.append(float(json[orig_i:i]))
